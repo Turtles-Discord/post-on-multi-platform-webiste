9e5614491c55302ff306239742ad5baf40ee2cfd
buyyaa <buyya32659@gmail.com>
2024-11-27 20:39:22 -0800

    final push for the links

--- client/src/components/TikTokVerification.js
+++ client/src/components/TikTokVerification.js
@@ -1,12 +1,11 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const TikTokVerification = () => {
   const { fileName } = useParams();
 
   useEffect(() => {
-    // Redirect to the correct file based on the fileName parameter
-    const fileUrl = `${process.env.PUBLIC_URL}/${fileName}.txt`; // Adjust the path as necessary
+    const fileUrl = `${process.env.PUBLIC_URL}/${fileName}.txt`;
     window.location.href = fileUrl;
   }, [fileName]);
 
