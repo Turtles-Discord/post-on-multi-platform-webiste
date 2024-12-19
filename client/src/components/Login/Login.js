fa96f812c580c86de7a222aaf348e21978e83702
buyyaa <buyya32659@gmail.com>
2024-12-19 14:56:20 -0800

    route in vercel for pages123

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { login } from '../../utils/api';
+import { login } from '../../utils/api';  
 import './Login.css';
 
 
