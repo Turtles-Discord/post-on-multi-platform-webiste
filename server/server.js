a5bae534d22a34b83e6155731e44207b3638c217
buyyaa <buyya32659@gmail.com>
2024-12-18 23:25:28 -0800

    fix the dash baord

--- server/server.js
+++ server/server.js
@@ -46,6 +46,14 @@ app.get('*', (req, res) => {
   });
 });
 
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
