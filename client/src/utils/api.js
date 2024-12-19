e628f39ca399102f0a54b2e932ce47b1f2d7ab68
buyyaa <buyya32659@gmail.com>
2024-12-18 22:45:25 -0800

    should fix the sign up error123123123123123

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,6 @@
-const API_URL = process.env.REACT_APP_API_URL;
-//const CLIENT_URL = process.env.REACT_APP_CLIENT_URL;
-
 import api from '../services/api';
+const API_URL = process.env.REACT_APP_API_URL;
+
 
 
 
