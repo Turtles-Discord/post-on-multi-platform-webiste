3460dbfa1188606205345f19a868c37b74d6bfe9
buyyaa <buyya32659@gmail.com>
2024-12-19 15:14:47 -0800

    added severy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-const API_URL = '/api';
-
 const api = axios.create({
-  baseURL: API_URL,
-  timeout: 300000,
+  baseURL: process.env.REACT_APP_API_URL || '/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
