b280afce8692119b9e6a47b928de1db0cdda4811
buyyaa <buyya32659@gmail.com>
2024-12-18 18:15:57 -0800

    fix 5

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { signup } from '../utils/api';
+import { signup } from '../../utils/api';
 import './Signup.css';
 
 function Signup() {
