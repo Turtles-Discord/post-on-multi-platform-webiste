ef28155e3d27f9d857c9aa2da6a8f981efa5c2eb
buyyaa <buyya32659@gmail.com>
2024-12-19 12:11:56 -0800

    singup link error fix

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://post-on-multi-platform-webiste.vercel.app',
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
