391d0d1f1d5f2749df880c2dfa86272982d0e9d2
buyyaa <buyya32659@gmail.com>
2024-12-19 19:43:15 -0800

    fix the peoblem

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const app = express();
 // Logging middleware
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
+  console.log('Request body:', req.body);
+  console.log('MongoDB connection state:', mongoose.connection.readyState);
   next();
 });
 
@@ -49,17 +51,23 @@ app.use((err, req, res, next) => {
   });
 });
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
+    
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
     console.error('MongoDB connection error:', err);
-  });
+    process.exit(1);
+  }
+};
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+startServer();
 
 module.exports = app;
